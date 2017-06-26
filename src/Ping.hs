aae509acfde74dbc70342553169e1dbfcef35adf
nicholasbrady <nick.brady@smartrac-group.com>
2017-06-26 09:43:16 -0700

    Open up a raw socket to access the IP layer directly

--- src/Ping.hs
+++ src/Ping.hs
@@ -1,7 +1,10 @@
 module Ping where
 
-import Network.Socket
+import Network.Socket (Family(AF_INET), SocketType(Raw), socket, getSocketName)
 
 helloWorld :: IO()
 helloWorld = do
+  sock <- socket AF_INET Raw 1
+  name <- getSocketName sock
+  _ <- putStrLn $ show name
   putStrLn "Hello World!"
