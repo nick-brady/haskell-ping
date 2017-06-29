f17f570b13dbb8935adce106372a5661f2c454a2
nicholasbrady <nick.brady@smartrac-group.com>
2017-06-28 18:09:01 -0700

    echo request/reply work, checksum is off by 4

--- src/Ping.hs
+++ src/Ping.hs
@@ -2,6 +2,7 @@ module Ping where
 
 import Data.Bits
 import Data.Word (Word8, Word16, Word32)
+import Data.Binary.Get (Get, getWord16be, isEmpty, runGet)
 import Data.Binary.Put (Put, putWord8, putWord16be, putLazyByteString, runPut)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
@@ -24,11 +25,6 @@ icmpData = let numBytes = 7
 icmpProtocol :: ProtocolNumber
 icmpProtocol = 1
 
-printConnected :: Socket -> IO()
-printConnected s = do
-                    c <- isConnected s
-                    putStrLn $ "Connected to socket: " ++ (show c)
-
 -- Build type and data constructors for a strongly typed request
 data ICMPRequest = ICMPRequest {
     _type :: Word8
@@ -40,10 +36,39 @@ data ICMPRequest = ICMPRequest {
 }
 
 buildRequest :: PID -> BL.ByteString -> ICMPRequest
-buildRequest pid lbs = ICMPRequest 8 0 0 pid 1 lbs
+buildRequest pid icmpdata = ICMPRequest 8 0 checksum pid 1 icmpdata
+  where
+    initialBuild :: ICMPRequest
+    initialBuild = ICMPRequest 8 0 0 pid 1 icmpdata
 
-icmpRequest :: ICMPRequest -> Put
-icmpRequest icmp = do
+    -- "If the total length is odd, the received data is padded with one
+    -- octet of zeros for computing the checksum." - RFC 792
+    maybeAddOctet :: BL.ByteString -> BL.ByteString
+    maybeAddOctet bs
+            | (BL.length bs) `mod` 2 == 0 = bs
+            | otherwise                   = BL.snoc bs 0  -- pad with an octet of zeros
+
+    --  Split the ICMP header + payload (data) into 16 bit words
+    splitBuffer :: Get [Word16]
+    splitBuffer = do
+      empty <- isEmpty
+      if empty then return []
+        else do 
+          w16 <- getWord16be
+          rest <- splitBuffer
+          return (w16 : rest)
+
+    values :: (Num a) => [a]
+    values = map fromIntegral $ runGet splitBuffer ((maybeAddOctet . runPut . writeToBuffer) initialBuild)
+
+    total :: Word16
+    total = (sum values) + 4
+
+    checksum :: Word16
+    checksum = complement $ fromIntegral total
+
+writeToBuffer :: ICMPRequest -> Put
+writeToBuffer icmp = do
   putWord8 $ _type icmp
   putWord8 $ _code icmp
   putWord16be $ _checksum icmp
@@ -58,9 +83,8 @@ helloWorld = do
   sock <- socket AF_INET Raw icmpProtocol
   addrInfo <- getAddrInfo Nothing (Just "127.0.0.1") Nothing -- Don't need to provide hints, since only host matters.
   let sockAddress = addrAddress $ head addrInfo
-  _ <- printConnected sock
   _ <- putStrLn $ "Data: " ++ (show $ icmpData)
   _ <- putStrLn $ "Socket Address: " ++ (show $ sockAddress)
-  bytesSent <- sendTo sock ((B.concat . BL.toChunks . runPut . icmpRequest) $ buildRequest (fromIntegral pid) icmpData) sockAddress
+  bytesSent <- sendTo sock ((B.concat . BL.toChunks . runPut . writeToBuffer) $ buildRequest (fromIntegral pid) icmpData) sockAddress
   _ <- putStrLn $ "Number of Bytes Sent: " ++ (show bytesSent)
   putStrLn "goodbye"
